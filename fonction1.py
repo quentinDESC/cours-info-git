07f0a4b3c42cbc1522612af8b7828d3229957298
Quentin Descamps <quentin.descamps@etu.minesparis.psl.eu>
2026-09-18 09:52:48 +0200

    devoirs pour le python: affichage

--- fonction1.py
+++ fonction1.py
@@ -12,3 +12,28 @@ def f3(x):
 def sqrt (x):
     return x**(1/2)
 
+import numpy as np
+a,b=np.indices((10,10))
+
+def rayures(n):
+    a,b=np.indices((n,n))
+    print (a%2)
+    
+    
+def damier(n):
+    a,b=np.indices((n,n))
+    print((a+b)%2)
+    
+
+def escalier(n):
+    a,b=np.indices((n,n))
+    print (a<b)
+    
+def damier_par_bloc(n):
+    a,b=np.indices((n,n))
+    a=a%3
+    b=b%3
+    a= (a==0)
+    b= (b==0)
+    c= a +b
+    print (c)
