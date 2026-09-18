3de2ca0ab0ec0533b3b926fa06537676e99a5fbe
Quentin Descamps <quentin.descamps@etu.minesparis.psl.eu>
2026-09-18 10:15:24 +0200

    suppression des trucs inutiles

--- fonction1.py
+++ fonction1.py
@@ -1,19 +1,5 @@
-def f(x):
-    return x+1
-
-def g(x): 
-    return 0
-def f2(x):
-    return 9
-
-def f3(x):
-    return 5
-
-def sqrt (x):
-    return x**(1/2)
-
 import numpy as np
-a,b=np.indices((10,10))
+
 
 def rayures(n):
     a,b=np.indices((n,n))
